4210b1f964acef85b8ab14e5c9732180b1512d83
William Kerby <w.kerby99@gmail.com>
2023-03-14 08:30:57 -0400

    confirm that card push works in index.html file

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const intern = require("./lib/Intern");
 const Intern = intern.Intern;
 
 //create flag for inquire process 
-var inquireOn = true;
+// var inquireOn = true;
 
 //create var to store number of cards created from function
 var cardCount = 1;
@@ -45,7 +45,8 @@ const managerCreate = () => {
     ]
     inquirer.prompt(managerQuestions).then(answers => {
         const newManager = new Manager(answers.managername, answers.managerid, answers.manageremail, answers.officenum);
-        console.log(`Hello! My name is ${newManager.name} and my office number is ${newManager.officeNumber}!`);
+        pushCard(newManager);
+        // console.log(`Hello! My name is ${newManager.name} and my office number is ${newManager.officeNumber}!`);
         doNext();
     })
 }
@@ -75,7 +76,8 @@ const engineerCreate = () => {
     ]
     inquirer.prompt(engineerQuestions).then(answers => {
         const newEngineer = new Engineer(answers.engineername, answers.engineerid, answers.engineeremail, answers.github);
-        console.log(`Hello! My name is ${newEngineer.name}, my id is ${newEngineer.id}, and my github username is ${newEngineer.github}!`);
+        pushCard(newEngineer);
+        // console.log(`Hello! My name is ${newEngineer.name}, my id is ${newEngineer.id}, and my github username is ${newEngineer.github}!`);
         doNext();
     })
 
@@ -106,7 +108,8 @@ const internCreate = () => {
     ]
     inquirer.prompt(internQuestions).then(answers => {
         const newIntern = new Intern(answers.internname, answers.internid, answers.internemail, answers.school);
-        console.log(`Hello! My name is ${newIntern.name}, my id is ${newIntern.id}, and I currently attend ${newIntern.school}!`);
+        pushCard(newIntern);
+        // console.log(`Hello! My name is ${newIntern.name}, my id is ${newIntern.id}, and I currently attend ${newIntern.school}!`);
         doNext();
     })
 };
@@ -123,7 +126,14 @@ const doNext = () => {
     ];
     inquirer.prompt(doNextQuestion).then(answers => {
         if (answers.next == "Finish building my team") {
-            inquireOn == false;
+            const finish =
+                `
+    </main>
+</body>
+</html>
+
+            `;
+            appendToFile('./dist/index.html', finish);
         }
 
         else if (answers.next == "Add an Engineer") {
@@ -136,18 +146,19 @@ const doNext = () => {
     })
 }
 
+//create function that appends string of data to file
+const appendToFile = (fileName, data) => {
+    fs.appendFile(fileName, data, e => e ? console.error(e) : console.log(""))
+}
 
 //create function that creates a card for employee objects
 const pushCard = (employee) => {//determines which specific pieces of information go in the card
     cardCount++; //increment card count by 1
+    var card;
     if (employee.getRole() == "Manager") {
-        const cardElem = document.querySelector("#card1");
-        const clone = cardElem.cloneNode(true); //will also copy all elements nested inside of it
-        clone.id = `card${cardCount}`;
-        console.log(clone.children); //want to access the divs inside of which name, role, id, and email info lives
-        //research traversing DOM to access the spans inside of the card
-        cardElem.after(clone);
-        `<div id="card1" class="card">
+        card =
+            `
+        <div id="card1" class="card">
             <div class="card-header">
                 <div id="name-role-info">
                     <div class="card-title-name">${employee.name}</div>
@@ -158,22 +169,57 @@ const pushCard = (employee) => {//determines which specific pieces of informatio
                 <div id="id-email-info">
                     <div class="card-data-id">ID: ${employee.id}</div>
                     <div class="card-data-email">Email: <span><a href="mailto:${employee.email}">${employee.email}</a></span></div>
-                    <div class="card-data-office">Office number: ${employee.officenum}
+                    <div class="card-data-office">Office number: ${employee.officeNumber}
                 </div>
             </div>
-        </div>`
+        </div>
+`;
     }
 
+    else if (employee.getRole() == "Engineer") {
+        card =
+            `
+        <div id="card1" class="card">
+            <div class="card-header">
+                <div id="name-role-info">
+                    <div class="card-title-name">${employee.name}</div>
+                    <div class="card-title-role">${employee.getRole()}</div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div id="id-email-info">
+                    <div class="card-data-id">ID: ${employee.id}</div>
+                    <div class="card-data-email">Email: <span><a href="mailto:${employee.email}">${employee.email}</a></span></div>
+                    <div class="card-data-github">GitHub: <span><a href="https://github.com/${employee.github}">${employee.github}</a></span></div>
+                </div>
+            </div>
+        </div>
+`;
+    }
+
+    else { //employee is an Intern
+        card =
+            `
+        <div id="card1" class="card">
+            <div class="card-header">
+                <div id="name-role-info">
+                    <div class="card-title-name">${employee.name}</div>
+                    <div class="card-title-role">${employee.getRole()}</div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div id="id-email-info">
+                    <div class="card-data-id">ID: ${employee.id}</div>
+                    <div class="card-data-email">Email: <span><a href="mailto:${employee.email}">${employee.email}</a></span></div>
+                    <div class="card-data-school">School: ${employee.school}</div>
+                </div>
+            </div>
+        </div>
+`;
+    }
+    appendToFile('./dist/index.html', card);
 
 }
-
-//create function that writes string of data to file
-const appendToFile = (fileName, data) => {
-    fs.appendFile(fileName, data, e => e ? console.error(e) : console.log("File created!"))
-}
-
-const mikeyG = new Manager("Mike Gotfryd", 89, 'mgotfryd@brasfieldgorrie.com', "103E");
-pushCard(mikeyG.getRole());
 
 //add block for user input
 managerCreate();
